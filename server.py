1d5281c86401c6819ade2e528b20d9d8448ca45b
Gnorbi951 <gnorbi951@gmail.com>
2019-11-02 14:55:45 +0100

    dropdown button functional

--- server.py
+++ server.py
@@ -1,10 +1,15 @@
-from flask import Flask, render_template
+from flask import Flask, render_template, redirect
 
 app = Flask(__name__)
 
 
 @app.route('/')
 def main_page():
+    return redirect('/question-with-one-answer')
+
+
+@app.route('/question-with-one-answer')
+def question_page():
     return render_template('index.html')
 
 
--- static/script.js
+++ static/script.js
@@ -1,1 +1,22 @@
-console.log('hey');
+const dropdownDiv = document.querySelector('#dropdown');
+const dropdownContext = `   <div id="js_drop">
+                            <input type="radio" checked="checked" id="yes_check" >Yes<br>
+                            <input type="radio" id="no_check">No<br>
+                            </div>`;
+const dropButton = document.querySelector('#drop');
+let dropDowStatus = 'not_toggled';
+
+function dropBar() {
+    if (dropDowStatus === 'not_toggled') {
+        let node = document.createRange().createContextualFragment(dropdownContext);
+        dropdownDiv.appendChild(node);
+        dropDowStatus = 'toggled';
+    } else {
+        let jsDropdown = document.querySelector('#js_drop');
+        jsDropdown.remove();
+        dropDowStatus = 'not_toggled';
+    }
+
+}
+
+dropButton.addEventListener('click', dropBar);
