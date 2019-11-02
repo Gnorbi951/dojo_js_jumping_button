59d7927cde261a25e0e583623ea94f6b8cac9a60
Gnorbi951 <gnorbi951@gmail.com>
2019-11-02 15:18:07 +0100

    Refactor

--- static/script.js
+++ static/script.js
@@ -1,33 +1,40 @@
-const dropdownDiv = document.querySelector('#dropdown');
-const dropdownContext = `   <div id="js_drop">
-                            <input type="radio" checked="checked" id="yes_check" >Yes<br>
-                            <input type="radio" id="no_check">No<br>
-                            </div>`;
-const dropButton = document.querySelector('#drop');
-const yesCheck = document.querySelectorAll('#yes_check');
-const noCheck = document.querySelectorAll('#no_check');
-let dropDowStatus = 'not_toggled';
+function main() {
 
-function dropBar() {
-    if (dropDowStatus === 'not_toggled') {
-        let node = document.createRange().createContextualFragment(dropdownContext);
-        dropdownDiv.appendChild(node);
-        dropDowStatus = 'toggled';
-    } else {
-        let jsDropdown = document.querySelector('#js_drop');
-        jsDropdown.remove();
-        dropDowStatus = 'not_toggled';
+
+    function dropBar() {
+        if (dropDowStatus === 'not_toggled') {
+            let node = document.createRange().createContextualFragment(dropdownContext);
+            dropdownDiv.appendChild(node);
+            dropDowStatus = 'toggled';
+        } else {
+            let jsDropdown = document.querySelector('#js_drop');
+            jsDropdown.remove();
+            dropDowStatus = 'not_toggled';
+        }
+
     }
 
-}
-
-function checkButtons(event) {
-    if (event.target.id === 'no_check') {
-        document.querySelector('#yes_check').checked = false;
-    } else if (event.target.id === 'yes_check') {
-        document.querySelector('#no_check').checked = false;
+    function checkButtons(event) {
+        if (event.target.id === 'no_check') {
+            document.querySelector('#yes_check').checked = false;
+        } else if (event.target.id === 'yes_check') {
+            document.querySelector('#no_check').checked = false;
+        }
     }
-}
 
-document.addEventListener('click', checkButtons);
-dropButton.addEventListener('click', dropBar);
+
+    const dropdownDiv = document.querySelector('#dropdown');
+    const dropdownContext = `   
+                        <div id="js_drop">
+                        <input type="radio" checked="checked" id="yes_check" >Yes<br>
+                        <input type="radio" id="no_check">No<br>
+                        </div>`;
+    const dropButton = document.querySelector('#drop');
+    const yesCheck = document.querySelectorAll('#yes_check');
+    const noCheck = document.querySelectorAll('#no_check');
+    let dropDowStatus = 'not_toggled';
+
+    document.addEventListener('click', checkButtons);
+    dropButton.addEventListener('click', dropBar);
+}
+main();
